ebeeeb9346fde4dc04198db46293360bdd71bcf1
sign <lijinbang@hhotel.com>
2019-05-26 18:42:44 +0800

    事务

--- react/demo1/src/components/transaction.js
+++ react/demo1/src/components/transaction.js
@@ -6,17 +6,27 @@ class Transaction {
     this.wrapper = wrapper
   }
   perform(anyMethod) {
-    this.wrapper.initialize()
+    this.wrapper.forEach(wrapper => wrapper.initialize())
     anyMethod.call()
-    this.wrapper.close()
+    this.wrapper.forEach(wrapper => wrapper.close())
   }
 }
-let transaction = new Transaction({
-  initialize() {
-    console.log("initialize")
+let transaction = new Transaction([
+  {
+    initialize() {
+      console.log("initialize")
+    },
+    close() {
+      console.log("close")
+    }
   },
-  close() {
-    console.log("close")
+  {
+    initialize() {
+      console.log("initialize")
+    },
+    close() {
+      console.log("close")
+    }
   }
-})
+])
 transaction.perform(setState)
