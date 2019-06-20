9de089db5d30cf14bf7fa0525c14939ae4238f28
sign <lijinbang@hhotel.com>
2019-06-20 17:28:47 +0800

    combineReducers封装

--- react/redux/src/redux/index.js
+++ react/redux/src/redux/index.js
@@ -1,3 +1,9 @@
 import createStore from "./createStore"
+import bindActionCreators from "./bindActionCreators"
+import combineReducers from "./combineReducers"
 
-export { createStore }
+export {
+  createStore, // 创建store
+  combineReducers, // 合并reducers
+  bindActionCreators // 把actionCreator和dispatch方法绑定在一起
+}
--- react/redux/src/store/reducers/index.js
+++ react/redux/src/store/reducers/index.js
@@ -2,7 +2,7 @@
 import counter from "./counter"
 import counter1 from "./counter1"
 import counter2 from "./counter2"
-import { combineReducers } from "redux"
+import { combineReducers } from "../../redux"
 
 let reducers = combineReducers({
   counter,
