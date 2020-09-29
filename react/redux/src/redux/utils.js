e04e69a1ac9da1ca190fdf8221a8323e550c20f7
sign_lee@163.com <sign_lee@163.com>
2020-09-29 10:48:47 +0800

    注释更新

--- react/redux/src/redux/utils.js
+++ react/redux/src/redux/utils.js
@@ -1,4 +1,4 @@
-//判断是否是一个纯对象
+//判断是否是一个纯对象（通过字面量的方式创建出来的才是纯对象，如果是以类的方式创建出来的，Object.getPrototypeOf(obj)还没有到Object的终点，还知道了当前构造函数那一层）
 export default function isPlainObject(obj) {
   if (typeof obj != "object" || obj === null) {
     return false
