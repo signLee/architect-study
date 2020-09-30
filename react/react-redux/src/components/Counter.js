a9f9349c16b1707c5f7d3f402c6ce90045b93250
sign_lee@163.com <sign_lee@163.com>
2020-09-30 12:54:26 +0800

    去除多余文件

--- react/react-redux/src/components/Counter.js
+++ react/react-redux/src/components/Counter.js
@@ -13,7 +13,7 @@ import {connect} from 'react-redux'
     )
   }
 }
-const mapStateToProps = state=>state
+const mapStateToProps = state=>({number:state.number*10})//之所以做一层中转的原因：1状态可能很大，需要用到的属性比较少  2.可能需要对属性做一些其它操作处理数据3.为了性能优化，可能会有其它非本组件的属性的变更导致当前组件的重新计算渲染
 const mapDispatchToProps = actions
 export default connect(
   mapStateToProps,
