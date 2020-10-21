8b17e7ccb406490e830fb86af70954656fe8e298
sign_lee@163.com <sign_lee@163.com>
2020-10-21 11:20:14 +0800

    redux-persist持久化

--- react/connected-react-router/src/index.js
+++ react/connected-react-router/src/index.js
@@ -4,22 +4,25 @@ import Home from './components/Home'
 import Counter from './components/Counter'
 import {Route,Link} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import store from './store'
+import {store,persistor} from './store'
 import {ConnectedRouter} from 'connected-react-router'
 // 1.ConnectedRouter可以对路由跳转进行拦截处理，进行跳转。2当路径改变的时候可以把当前的路径信息存放到仓库里去
 import history from './store/history';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <>
-        <Link to="/">Home</Link>
-        <Link to="/counter">Counter</Link>
-        <hr/>
-        <Route path="/" exact={true} component={Home}></Route>
-        <Route path="/counter" component={Counter}></Route>
-      </>
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/counter">Counter</Link>
+          <hr/>
+          <Route path="/" exact={true} component={Home}></Route>
+          <Route path="/counter" component={Counter}></Route>
+        </>
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- react/connected-react-router/src/store/index.js
+++ react/connected-react-router/src/store/index.js
@@ -3,9 +3,20 @@ import reduces from './reduces';
 import {routerMiddleware} from 'connected-react-router';
 import history from './history'
 
-let store = createStore(reduces,applyMiddleware(routerMiddleware(history)))
+import { persistStore, persistReducer } from 'redux-persist'//redux-persist的原理是每一次修改了store后同步到storage里去
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, reduces)
+let store = createStore(persistedReducer,applyMiddleware(routerMiddleware(history)))
 // 上面的写法等同于如下
 // let store = applyMiddleware(routerMiddleware(history))(createStore)(reduces)
 window.store = store
-
-export default store
+let persistor = persistStore(store)
+export {
+  store,
+  persistor
+}
