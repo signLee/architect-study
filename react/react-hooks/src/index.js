71fc381dea1f012548f242a39bdb507ecc7ed153
sign_lee@163.com <sign_lee@163.com>
2020-11-10 20:24:44 +0800

    自定义hooks

--- react/react-hooks/src/index.js
+++ react/react-hooks/src/index.js
@@ -1,22 +1,41 @@
 import React,{useEffect,useState,createRef,useRef,forwardRef,useImperativeHandle} from 'react';
 import ReactDOM from 'react-dom';
-
-class Counter extends React.Component{
-  state = { number:0 }
-  componentDidMount(){
+// 如果说一个函数以use开头，并且在函数内使用了hooks，那么这就是一个自定义hooks
+function useNumber(){
+  let [number,setNumber] = useState(0)
+  useEffect(()=>{
     setInterval(()=>{
-      this.setState({number:this.state.number+1}) 
+      setNumber(number=>number+1)
     },1000)
-  }
-  render(){
-    return this.props.render({number:this.state.number})
-  }
+  },[])
+  return [number,setNumber]
 }
-
+function Counter1(){
+  let [number,setNumber] = useNumber()//自定义hooks只能在函数式组件里使用
+  return (
+    <>
+    <div>{number}</div>
+    <button onClick={()=>{
+      setNumber(number+1)
+    }}>+</button>
+    </>
+  )
+}
+function Counter2(){
+  let [number,setNumber] = useNumber()//自定义hooks只能在函数式组件里使用两个组件的state是独立的
+  return (
+    <>
+    <div>{number}</div>
+    <button onClick={()=>{
+      setNumber(number+1)
+    }}>+</button>
+    </>
+  )
+}
 ReactDOM.render(
   <>
-    <Counter render={props=><p>{props.number}</p>}></Counter>
-    <Counter render={props=><button>{props.number}</button>}></Counter>
+    <Counter1></Counter1>
+    <Counter2></Counter2>
   </>,
   document.getElementById('root')
 );
